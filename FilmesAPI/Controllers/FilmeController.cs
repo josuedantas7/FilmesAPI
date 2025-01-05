cde2ac369e5a3ee051282c8a82378557c1d739fc
Josué Dantas <105951027+josuedantas7@users.noreply.github.com>
2025-01-05 19:55:38 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "Titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+
+
+}
