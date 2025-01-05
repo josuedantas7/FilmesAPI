bc118cf42e7073f80d67010abf4f08f089340973
Josué Dantas <105951027+josuedantas7@users.noreply.github.com>
2025-01-05 20:23:32 -0300

    Refatoração e melhorias no gerenciamento de filmes
    - Adição de um campo estático `id` para gerenciar IDs únicos para os filmes.
    - Modificação do método `AdicionarFilme` para atribuir um ID único a cada filme adicionado.
    - Alteração do método `RecuperaFilmes` para aceitar parâmetros de consulta `skip` e `take`, permitindo paginação dos resultados.
    - Adição do método `RecuperarFilmePorId` para recuperar um filme específico pelo seu ID.
    - Adição da propriedade `Id` na classe `Filme` para armazenar o ID único de cada filme.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using FilmesAPI.Models;
+﻿using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -11,19 +10,24 @@ public class FilmeController : ControllerBase
 {
 
     private static List<Filme> filmes = new List<Filme>();
-
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes()
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,[FromQuery] int take = 50)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperarFilmePorId(int Id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == Id);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "Titulo do filme é obrigatório")]
     public string Titulo { get; set; }
     [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
@@ -12,6 +13,4 @@ public class Filme
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
-
-
 }
